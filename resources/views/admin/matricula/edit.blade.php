8d4f2703a672274d6565f610e88f22b604cb2877
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-09 13:32:26 -0500

    mod

--- resources/views/admin/matricula/edit.blade.php
+++ resources/views/admin/matricula/edit.blade.php
@@ -128,7 +128,7 @@
             <!--mostrar imagen-->
                 @if ($matricula->comprobante_imagen != "")
                 <div class="mb-3"> 
-                    <img src="{{asset($matricula->comprobante_imagen) }}" alt="">
+                    <img src="{{asset($matricula->comprobante_imagen) }}" alt="" width="400px">
                 </div>
                 @endif
             <!--comprobante imagen-->
