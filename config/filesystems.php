12a8d0b54a0f7b32d8a2c2b6989c8a80e0af7da4
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-09 11:52:09 -0500

    modificacion jj

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public_html'),
             'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
+            'visibility' => 'public_html',
         ],
 
         's3' => [
