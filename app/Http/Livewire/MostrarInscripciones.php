7683cf06cc240d9fb3ad141ca3eaa272692892fb
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-14 15:17:03 -0500

    mod

--- app/Http/Livewire/MostrarInscripciones.php
+++ app/Http/Livewire/MostrarInscripciones.php
@@ -24,7 +24,7 @@ class MostrarInscripciones extends Component
         ->orwhere('dni','like','%' . $this->search.'%')
         ->orwhere('phone','like','%' . $this->search.'%')
         ->orderBy($this->sort, $this->direction)
-        ->paginate(10);
+        ->paginate(300);
         $matriculas = Matricula::all();
         return view('livewire.mostrar-inscripciones',compact('inscripciones','matriculas'));
     }
