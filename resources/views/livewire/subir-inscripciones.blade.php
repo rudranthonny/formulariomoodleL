b7913bd7893efe082537c292067fd3be4aeae19a
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-13 22:50:51 -0500

    mod

--- resources/views/livewire/subir-inscripciones.blade.php
+++ resources/views/livewire/subir-inscripciones.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button type="button" class="btn btn-warning" data-toggle="modal2" data-target="#ventanaModal2" style="color : white">
+    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#ventanaModal2" style="color : white">
         <i class="fas fa-upload"></i> Subir Inscripciones
     </button>
 <div wire:ignore.self class="modal face" id="ventanaModal2" tabindex="-2" role="dialog" aria-labelledby="tituloVentana2" aria-hidden="true">
@@ -7,7 +7,7 @@
         <div class="modal-content">    
                 <div class="modal-header">
                     <h5 id="tituloVentana2">Subir Usuarios</h5>
-                    <button class="close" data-dismiss="modal2" arial-label="cerrar">
+                    <button class="close" data-dismiss="modal" arial-label="cerrar">
                         <span aria-hidden="true">
                             &times;
                         </span>
@@ -21,10 +21,9 @@
                     <div class="alert alert-danger text-end"  wire:loading wire:target="inscripciones" style="display:none">
                         por favor espere
                     </div>
-                    <strong>{{$resultado2}}</strong> 
-                    <p><a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vS4C3c8lCJ2a0_yO8cWYQD20xWq98hch0KhodXzjwzAQgnRGeVRyk3hL2t8k5is8fQyyzerGvOtaYM5/pubhtml" target="_blank">Fomato para subir inscripciones "Clic Aqu&iacute;"</a></p>                     
+                    <strong>{{$resultado2}}</strong>                      
                 </div>
-                <div class="modal2-footer">
+                <div class="modal-footer">
                     <button class="btn btn-danger" data-dismiss="modal2">
                         Cerrar
                     </button>
