e0182548edbf60a9a3c54854756b444368406f2a
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-14 10:30:44 -0500

    ,mmod

--- resources/views/livewire/registrar-usuarios.blade.php
+++ resources/views/livewire/registrar-usuarios.blade.php
@@ -17,6 +17,7 @@
                     <div class="mb-3">
                         <label for="email" class="form-label">email</label>
                         <input type="email" class="form-control" id="email" aria-describedby="emailHelp" wire:model="email">
+                        {{$email}}
                         @error('email')
                             <span>{{$message}}</span>
                         @enderror
