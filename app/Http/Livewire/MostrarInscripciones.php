b140c1ce7e51cd50e723365b25fc69e44f7aad68
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-18 21:29:14 -0500

    mod

--- app/Http/Livewire/MostrarInscripciones.php
+++ app/Http/Livewire/MostrarInscripciones.php
@@ -98,7 +98,7 @@ class MostrarInscripciones extends Component
             $inscripciones = DB::table('inscripcions')
             ->where('inicio_id',$this->binicio)
             ->whereNotExists(function ($query) {
-                $query->select("NULL")
+                $query->select('NULL')
                       ->from('matriculas')
                       ->whereColumn('inscripcions.user_id', 'matriculas.user_id');
             })->get();
