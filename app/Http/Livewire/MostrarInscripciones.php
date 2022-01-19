3026575193f1df17e5436cd0aaff0c0f77817e04
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-18 23:06:37 -0500

    mod

--- app/Http/Livewire/MostrarInscripciones.php
+++ app/Http/Livewire/MostrarInscripciones.php
@@ -107,11 +107,7 @@ class MostrarInscripciones extends Component
                 $query->select(DB::raw('NULL'))
                       ->from('matriculas')
                       ->whereColumn('inscripcions.user_id', 'matriculas.user_id');
-            })->get();
-            dd($inscripciones);
-            $inscripciones = Inscripcion::where('name','like','%' . $this->search.'%')
-            ->where('inicio_id',$this->binicio)
-            ->paginate($this->blista);
+            })->get()->paginate($this->blista);
         }
         else{
             $inscripciones = Inscripcion::where('name','like','%' . $this->search.'%')
