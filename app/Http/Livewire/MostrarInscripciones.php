99b13821820943265ce30d2c0067e6c4f61a6fca
Anthonny <ing.anthonny.joel@gmail.com>
2022-01-20 18:18:02 -0500

    mod

--- app/Http/Livewire/MostrarInscripciones.php
+++ app/Http/Livewire/MostrarInscripciones.php
@@ -25,7 +25,7 @@ class MostrarInscripciones extends Component
     public $bmatriculado;
     public $binicio;
     public $bestado;
-    public $bgente;
+    public $bagente;
     public $iteration;
     public $blista="30";
     public $sort = "id";
