ddb512a60fa0ecd2b47178fdceaf830023f84df2
Anthonny <ing.anthonny.joel@gmail.com>
2022-03-14 20:07:18 -0500

    mod

--- resources/views/livewire/reporte-cajero.blade.php
+++ resources/views/livewire/reporte-cajero.blade.php
@@ -3,15 +3,33 @@
         <thead>
             <tr class="bg-dark">
               <th scope="col">ESTUDIANTE</th>
+              <th scope="col">COSTO</th>
+              <th scope="col">AGENTE</th>
+              <th scope="col">TIPO</th>
+              <th scope="col">FECHA DE PAGO</th>
+              <th scope="col">COMPROBANTE</th>
+              <th scope="col">REFERENCIA</th>
             </tr>
         </thead>
         <tbody>
-            {{$matriculas}}
             @foreach ($matriculas as $matricula)
             <tr>
+                <td>{{$matricula->name." ".$matricula->lastname}}</td>
+                <td>{{$matricula->costo}}</td>
+                <td>{{$matricula->agente}}</td>
+                <td>{{$matricula->tipo}}</td>
+                <td>{{$matricula->fechapago}}</td>
+                <td>{{$matricula->comprobannte}}</td>
+                <td>{{$matricula->comprobannte}}</td>
+                @if ($matricula->comprobante_imagen)
                 <td>
-                    {{$matricula->name." ".$matricula->lastname}}
+                  <a href="{{asset($matricula->comprobante_imagen)}}" target="_blank">ver</a>   
                 </td>
+                @else
+                <td>
+                    no tiene  
+                </td>
+                @endif
             <tr> 
             @endforeach
         </tbody>
