c3595c2a97ca063e2964329992e8dbe9ca199e67
Anthonny <ing.anthonny.joel@gmail.com>
2022-03-21 22:11:53 -0500

    mod

--- app/Http/Livewire/ShowMatriculas2.php
+++ app/Http/Livewire/ShowMatriculas2.php
@@ -3,10 +3,61 @@
 namespace App\Http\Livewire;
 
 use App\Models\Matricula;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class ShowMatriculas2 extends Component
 {
+    public $iteration;
+    
+    public function editarmatricula($id_matricula){
+        $ematricula = Matricula::find($id_matricula);
+        $this->eid = $ematricula->id;
+        $this->ename = $ematricula->name;
+        $this->elastname = $ematricula->lastname;
+        $this->euser_id = $ematricula->user_id;
+        $this->ecosto = $ematricula->costo;
+        $this->eagente = $ematricula->agente;
+        if ($ematricula->tipo == null) {
+         $this->etipo = "Soles";
+        }
+        else{
+         $this->etipo = $ematricula->tipo;
+        }
+        $this->efechapago = $ematricula->fechapago;
+        $this->ecomprobante  = $ematricula->comprobante;
+        $this->ecomprobante_imagen = $ematricula->comprobante_imagen;
+        $this->eprograma_id = $ematricula->programa_id;
+        //clean up
+        $this->ecomprobante_imagen_file = null;
+        $this->iteration++;
+     }
+
+     public function actualizarmatriculas(){
+        $mmatricula = Matricula::find($this->eid);
+        $mmatricula->costo = $this->ecosto;
+        $mmatricula->agente = $this->eagente;
+        $mmatricula->tipo =$this->etipo;
+        $mmatricula->fechapago = $this->efechapago;
+        $mmatricula->comprobante = $this->ecomprobante;
+        $mmatricula->cajero_id = auth()->user()->id;
+        $mmatricula->save();
+        //actualizar el comprobante_imagen de la tabla matricula
+        $mmatricula = Matricula::find($this->eid);
+        if($this->ecomprobante_imagen_file != null)
+        {
+                $extension = $this->ecomprobante_imagen_file->extension();
+                $eliminar = str_replace('storage','public',$mmatricula->comprobante_imagen);
+                Storage::delete([$eliminar]);
+                $imagenenu = $this->ecomprobante_imagen_file->storeAs('public/comprobantes',$mmatricula->user_id."-".rand(1,2000).str_replace(' ', '', $mmatricula->comprobante).".".$extension);
+                $url = Storage::url($imagenenu);
+                $mmatricula->update(['comprobante_imagen' => $url]);
+                $this->ecomprobante_imagen = $mmatricula->comprobante_imagen;
+                $this->ecomprobante_imagen_file = null;
+                $this->iteration++;
+        }
+    }
+    
     public function render()
     {
         $matriculas = Matricula::all()->where('cajero_id',auth()->user()->id);
--- resources/views/livewire/show-matriculas2.blade.php
+++ resources/views/livewire/show-matriculas2.blade.php
@@ -47,9 +47,104 @@
                     <td>
                         {{$matricula->agente}}
                     </td>
+                    <td><a class="btn btn-success" data-toggle="modal" data-target="#ventanaModal3" wire:click="editarmatricula({{$matricula->id}})"><i class="fas fa-edit"></i></a></td>
                 </tr>  
                 @endforeach
             </tbody>
         </table>
     </div>
+     <!--ventana editar-->
+   <div wire:ignore.self class="modal face" id="ventanaModal3" tabindex="-2" role="dialog" aria-labelledby="tituloVentana3" aria-hidden="true" >
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">    
+                <div class="modal-header">
+                    <h5 id="tituloVentana3">Modificar Matricula</h5>
+                    <button class="close" data-dismiss="modal" arial-label="cerrar">
+                        <span aria-hidden="true">
+                            &times;
+                        </span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!--Agencia-->
+                  <div class="mb-3">
+                    <label for="agente" class="form-label">Agente</label>
+                    <select class="form-select" name="agente" aria-label="Default select example" wire:model.defer="eagente">
+                    <option value="BancoNacion">Banco de la Nación</option> 
+                    <option value="BBVA">BBVA</option>
+                    <option value="BCP">BCP</option>  
+                    <option value="DirectoPago">Directo Pago</option>  
+                    <option value="Interbank">Interbank</option>  
+                    <option value="MoneyGram">MoneyGram</option>  
+                    <option value="Paypal">Paypal</option>   
+                    <option value="Plim">Plim</option>  
+                    <option value="Scotiabank">Scotiabank</option>  
+                    <option value="WesterUnion">Wester Unión</option>   
+                    <option value="Yape">Yape</option>       
+                    </select>
+                    @error('agente')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                  </div>
+                  <!--tipo de soles-->
+                  <div class="mb-3">
+                  <label for="tipo" class="form-label">Tipo</label>
+                  <select class="form-select" name="tipo" aria-label="Default select example" wire:model.defer="etipo">
+                  <option value="Soles" selected>Soles</option>    
+                  <option value="Dolares">Dolares</option>
+                  </select>  
+                  @error('tipo')
+                  <span class="text-danger">{{$message}}</span>
+                  @enderror
+                  </div>
+                  <!--monto-->
+                  <div class="mb-3">
+                  <label for="costo" class="form-label">Costo</label>
+                  <input type="text" class="form-control" id="costo" name="costo" wire:model.defer="ecosto">
+                    @error('costo')
+                    <span class="text-danger">{{$message}}</span>
+                    @enderror
+                  </div>
+                  <!--fecha-->
+                  <div class="mb-3">
+                  <label for="fechapago" class="form-label">Fecha</label>
+                  <input type="date" class="form-control" id="fechapago" name="fechapago" wire:model.defer="efechapago">
+                  @error('fechapago')
+                  <span class="text-danger">{{$message}}</span>
+                  @enderror
+                  </div>
+           <!--numero de comprobante-->
+           <div class="mb-3">
+              <label for="comprobante" class="form-label">Numero de Comprobante</label>
+              <input type="text" class="form-control" id="ncomprobante" name="comprobante" wire:model.defer="ecomprobante">
+              @error('comprobante')
+              <span class="text-danger">{{$message}}</span>
+              @enderror
+          </div>
+          <!--mostrar imagen-->
+              @if ($ecomprobante_imagen != "")
+              <div class="mb-3"> 
+                  <center><img src="{{asset($ecomprobante_imagen) }}" alt="" width="300px"></center>
+              </div>
+              @endif
+          <!--comprobante imagen-->
+          <div class="mb-3">
+              <input type="file" name="comprobante_imagen" accept="image/*" wire:model="ecomprobante_imagen_file" id="upload{{$iteration}}">
+              @error('ecomprobante_imagen_file')
+                  <span class="text-danger">{{$message}}</span>
+              @enderror
+          </div>
+          <!---->
+          </div>
+          <div class="modal-footer">
+                    <button class="btn btn-danger" data-dismiss="modal">
+                        Cerrar
+                    </button>
+                    <button class="btn btn-success" wire:loading.attr="disabled" wire:target="save, matriculas" type="button" wire:click="actualizarmatriculas">
+                        Modificar Matricula
+                    </button>
+          </div>
+        </div>
+    </div>
+  </div>
 </div>
