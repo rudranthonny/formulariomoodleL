1e86e52178bb50e68182f0cae63135e466bb9957
Anthonny <ing.anthonny.joel@gmail.com>
2022-03-22 07:50:56 -0500

    mod

--- app/Http/Livewire/ShowMatriculas2.php
+++ app/Http/Livewire/ShowMatriculas2.php
@@ -86,8 +86,8 @@ class ShowMatriculas2 extends Component
     public function render()
     {
         if($this->fecha_inicio != null and $this->fecha_final != null){
-            $matriculas = Matricula::where('cajero_id',auth()->user()->id)->where('name','like','%' .$this->search2.'%')->where('fechapago','>=',$this->fecha_inicio)->where('fechapago','<=',$this->fecha_final)
-            ->orwhere('cajero_id',auth()->user()->id)->where('lastname','like','%' .$this->search2.'%')->where('fechapago','>=',$this->fecha_inicio)->where('fechapago','<=',$this->fecha_final)
+            $matriculas = Matricula::where('cajero_id',auth()->user()->id)->where('name','like','%' .$this->search2.'%')->wheredate($this->fecha_inicio,'>=','fechapago')->wheredate($this->fecha_final,'<=','fechapago')
+            ->orwhere('cajero_id',auth()->user()->id)->where('lastname','like','%' .$this->search2.'%')->wheredate($this->fecha_inicio,'>=','fechapago')->wheredate($this->fecha_final,'<=','fechapago')
             ->paginate(10);
         }
         else{
