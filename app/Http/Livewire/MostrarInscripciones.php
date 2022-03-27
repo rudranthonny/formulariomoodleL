fe095cfbe89077cd2559216d9eaa4bdba37274ea
Anthonny <ing.anthonny.joel@gmail.com>
2022-03-27 13:06:03 -0500

    mod

--- app/Http/Livewire/MostrarInscripciones.php
+++ app/Http/Livewire/MostrarInscripciones.php
@@ -202,7 +202,6 @@ class MostrarInscripciones extends Component
                   ->whereColumn('inscripcions.id', 'matriculas.inscripcion_id')
                   ->where('matriculas.programa_id',$this->bprograma);
         })->get();
-        dd($this->inscripciones);
         }
         /*-------------------------*/
        if ($this->bmatriculado == "matriculados"  && $this->binicio != null && $this->bprograma != "no" && $this->bestado=="pagante" && $this->bagente == "") {
